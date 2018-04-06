9fb3474cad68479e147251faf4ff159237d86b1d
akash purohit <apurohit@edgewaternetworks.com>
2018-04-06 20:32:10 +0530

    predict done

--- src/app/play/match-overview/match-overview.component.ts
+++ src/app/play/match-overview/match-overview.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlayService } from '../../core/services/play.service';
 import { AbstractRedirect } from '../../core/abstract-redirect';
 import { SecurityService } from '../../core/security.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-match-overview',
@@ -11,24 +11,34 @@ import { Router } from '@angular/router';
 })
 export class MatchOverviewComponent extends AbstractRedirect implements OnInit {
 
-  public match = {};
+  public match;
+  public matchOverview;
 
   constructor(
     public securityService: SecurityService,
     public router: Router ,
-    public playService: PlayService
+    public playService: PlayService,
+    private route: ActivatedRoute
   ) {
     super(securityService, router);
   }
 
   ngOnInit() {
     super.ngOnInit();
-    this.getMatchOverview();
+    const id = +(this.route.snapshot.params.id || 0);
+    this.getMatchOverview(id);
+    this.getMatchDetail(id);
   }
 
-  public getMatchOverview() {
-    this.playService.getMatchOverview(1).subscribe(match => {
+  public getMatchDetail(id) {
+    this.playService.getMatchById(id).subscribe(match => {
       this.match = match;
+    });
+  }
+
+  public getMatchOverview(id) {
+    this.playService.getMatchOverview(id).subscribe(match => {
+      this.matchOverview = match;
     });
   }
 
--- src/app/play/predict/predict.component.ts
+++ src/app/play/predict/predict.component.ts
@@ -3,7 +3,9 @@ import { PlayService } from '../../core/services/play.service';
 import { SecurityService } from '../../core/security.service';
 import { AbstractRedirect } from '../../core/abstract-redirect';
 import { Router, ActivatedRoute } from '@angular/router';
-
+import * as _ from 'lodash';
+declare var jquery: any;
+declare var $: any;
 @Component({
   selector: 'app-predict',
   templateUrl: './predict.component.html',
@@ -11,7 +13,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class PredictComponent extends AbstractRedirect implements OnInit {
 
-  public match = {};
+  public match;
+  public contests;
+  public prediction;
+  public checkedContests = [];
+  public teamContestMap = [];
 
   constructor(
     public securityService: SecurityService,
@@ -26,6 +32,7 @@ export class PredictComponent extends AbstractRedirect implements OnInit {
     super.ngOnInit();
     const id = +(this.route.snapshot.params.id || 0);
     this.getMatch(id);
+    this.getContests();
   }
 
   private getMatch(id): void {
@@ -33,6 +40,45 @@ export class PredictComponent extends AbstractRedirect implements OnInit {
       .subscribe(res => {
         this.match = res;
       });
+  }
+
+  public getContests(): void {
+    this.playService.getContests()
+      .subscribe(res => {
+        this.contests = res;
+      });
+  }
+
+  public predictIt(): void {
+    // this.
+  }
+
+  // id: contestId
+  // event.target.value : teamid
+  public teamChanged(id, event) {
+    const index = _.indexOf(this.teamContestMap, _.find(this.teamContestMap, { contestId: id }));
+    if (index !== -1) {
+      this.teamContestMap[index].teamId = +event.target.value;
+    }
+  }
+
+  public contestChanged(id, event) {
+    if (event.target.checked) {
+      this.checkedContests.push(id);
+      this.teamContestMap.push({'contestId': id, 'teamId': this.match.team1.id});
+    } else {
+        $('#cont-' + id).prop('selectedIndex', 0);
+      _.pull(this.checkedContests, id);
+      const index = _.indexOf(this.teamContestMap, _.find(this.teamContestMap, { contestId: id }));
+      this.teamContestMap.splice(index, 1);
+    }
+  }
+
+  public checkDisable(id): boolean {
+    if ((_.indexOf(this.checkedContests, id)) !== -1 ) {
+      return false;
+    }
+    return true;
   }
 
 }
